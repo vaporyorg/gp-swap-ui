76cc7d87f1c388a945e92ed37e20059e21a3a8c2
Anxo Rodriguez <anxolin@gmail.com>
2020-12-03 14:29:30 +0100

    Add proxy to hook

--- src/custom/state/swap/hooks.ts
+++ src/custom/state/swap/hooks.ts
@@ -1,6 +1,22 @@
+import { Field } from '@src/state/swap/actions'
+import { Currency, CurrencyAmount, Trade } from '@uniswap/sdk'
+import { useDerivedSwapInfo as useDerivedSwapInfoUniswap } from '@src/state/swap/hooks'
+
 export {
   useDefaultsFromURLSearch,
-  useDerivedSwapInfo,
+  useDerivedSwapInfo as useDerivedSwapInfoUniswap,
   useSwapActionHandlers,
   useSwapState
 } from '@src/state/swap/hooks'
+
+// from the current swap inputs, compute the best trade and return it.
+export function useDerivedSwapInfo(): {
+  currencies: { [field in Field]?: Currency }
+  currencyBalances: { [field in Field]?: CurrencyAmount }
+  parsedAmount: CurrencyAmount | undefined
+  v2Trade: Trade | undefined
+  inputError?: string
+  v1Trade: Trade | undefined
+} {
+  return useDerivedSwapInfoUniswap()
+}
